23dee1a361b7111a340ad9c14da372a9cdaa985a
Alex Ninneman alex@ninneman.org <alex@ninneman.org>
2016-11-13 23:52:14 -0600

    added slides and demo app, need to create kube files

---
+++ demo/middlewares/middlewares.go
@@ -0,0 +1,33 @@
+// Package middlewares contains gin middlewares
+// Usage: router.Use(middlewares.Connect)
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ninnemana/devgroup-11-15-16/demo/db"
+)
+
+// Connect middleware clones the database session for each request and
+// makes the `db` object available for each handler
+func Connect(c *gin.Context) {
+	s := db.Session.Clone()
+
+	defer s.Close()
+
+	c.Set("db", s.DB(db.Mongo.Database))
+	c.Next()
+}
+
+// ErrorHandler is a middleware to handle errors encountered during requests
+func ErrorHandler(c *gin.Context) {
+	c.Next()
+
+	// TODO: Handle it in a better way
+	if len(c.Errors) > 0 {
+		c.HTML(http.StatusBadRequest, "400", gin.H{
+			"errors": c.Errors,
+		})
+	}
+}
